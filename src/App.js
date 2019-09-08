5152a8f57e034f957f0feb4771e33db485623895
Stephanie Weber <sweber@thoughtworks.com>
2019-09-07 20:52:35 -0700

    Remove index redirect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import CertModal from "./Modal/CertModal";
 
 class App extends Component {
@@ -8,7 +8,6 @@ class App extends Component {
     render() {
     return (
         <BrowserRouter>
-            <Redirect exact from="/" to="/0" />
                 <div className="App">
                 <div className="App-header">
                   <h2>Thank you for participating in Virgil Con!</h2>
